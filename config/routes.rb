fb5971514dc57fdf7b6c768c2d1d78cede356c6e
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-03 21:59:06 -0600

    added actions to posts controller, added stuff to indes.html.rb, show.html.rb, new.html.rb, added more actions to posts controller, did some route stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+ 
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about', as: :about
+
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
